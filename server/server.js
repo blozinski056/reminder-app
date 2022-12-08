85471696e117fcbf514657e9a1d241eb93715e54
blozinski056 <bloz056111@gmail.com>
2022-12-08 13:22:46 -0800

    edited express app.listen

--- server/server.js
+++ server/server.js
@@ -128,6 +128,6 @@ app.delete("/users/:username/notes/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000...");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server started...");
 });
