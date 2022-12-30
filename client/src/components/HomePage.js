1af76b6005aa8530073641d3d2553006a095fd2b
blozinski056 <bloz056111@gmail.com>
2022-12-30 10:31:45 -0800

    added media queries and adjusted styles

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -96,56 +96,58 @@ export default function HomePage() {
 
   return (
     <section className="homepage">
-      <div className="homepage-title">RemindMe</div>
+      <h1 className="homepage-title">RemindMe</h1>
 
-      <div className="homepage-login">
-        <h1 className="login-title">LOG IN</h1>
-        <form className="login-form" onSubmit={(e) => login(e)}>
-          <h5 className="login-incorrect">
-            {loginError && "Incorrect username/password."}
-          </h5>
-          <input
-            className="login-username"
-            placeholder="Username"
-            required="required"
-          />
-          <input
-            className="login-password"
-            placeholder="Password"
-            type="password"
-            required="required"
-          />
-          <input className="login-button" type="submit" value="Log In" />
-        </form>
-      </div>
+      <div className="form-container">
+        <div className="homepage-login">
+          <h1 className="login-title">LOG IN</h1>
+          <form className="login-form" onSubmit={(e) => login(e)}>
+            <h5 className="login-incorrect">
+              {loginError && "Incorrect username/password."}
+            </h5>
+            <input
+              className="login-username"
+              placeholder="Username"
+              required="required"
+            />
+            <input
+              className="login-password"
+              placeholder="Password"
+              type="password"
+              required="required"
+            />
+            <input className="login-button" type="submit" value="Log In" />
+          </form>
+        </div>
 
-      <div className="homepage-signup">
-        <h1 className="login-title">SIGN UP</h1>
+        <div className="homepage-signup">
+          <h1 className="login-title">SIGN UP</h1>
 
-        <form className="signup-form" onSubmit={(e) => signup(e)}>
-          <h5 className="signup-incorrect">
-            {signupError === 1 && "Username already exists!"}
-            {signupError === 2 && "Passwords do not match!"}
-          </h5>
-          <input
-            className="signup-username"
-            placeholder="Username"
-            required="required"
-          />
-          <input
-            className="signup-password"
-            placeholder="Password"
-            type="password"
-            required="required"
-          />
-          <input
-            className="signup-password-confirm"
-            placeholder="Confirm Password"
-            type="password"
-            required="required"
-          />
-          <input className="signup-button" type="submit" value="Sign Up" />
-        </form>
+          <form className="signup-form" onSubmit={(e) => signup(e)}>
+            <h5 className="signup-incorrect">
+              {signupError === 1 && "Username already exists!"}
+              {signupError === 2 && "Passwords do not match!"}
+            </h5>
+            <input
+              className="signup-username"
+              placeholder="Username"
+              required="required"
+            />
+            <input
+              className="signup-password"
+              placeholder="Password"
+              type="password"
+              required="required"
+            />
+            <input
+              className="signup-password-confirm"
+              placeholder="Confirm Password"
+              type="password"
+              required="required"
+            />
+            <input className="signup-button" type="submit" value="Sign Up" />
+          </form>
+        </div>
       </div>
     </section>
   );
--- client/src/components/ReminderWall.js
+++ client/src/components/ReminderWall.js
@@ -1,18 +1,11 @@
-import React from "react"
+import React from "react";
 
-export default function ReminderWall({tiles}) {
-
+export default function ReminderWall({ tiles }) {
   return (
-    <section>
-      {tiles.length === 0 &&
-        <h1 className="empty">No more reminders!</h1>
-      }
-      
-      {tiles.length > 0 &&
-        <div className="tile-container">
-          {tiles}
-        </div>
-      }
+    <section className="reminder-wall">
+      {tiles.length === 0 && <h1 className="empty">No more reminders!</h1>}
+
+      {tiles.length > 0 && <div className="tile-container">{tiles}</div>}
     </section>
-  )
+  );
 }
